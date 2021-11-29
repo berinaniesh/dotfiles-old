3ebf5837f73fd68bbec7b09bf2e6affdb32bcc4e
= <=>
2021-11-29 16:35:07 +0530

    update

--- user/fish/.config/fish/config.fish
+++ user/fish/.config/fish/config.fish
@@ -8,3 +8,4 @@ set -Ux PATH $PATH /home/berinaniesh/.golib/bin
 abbr ytda "youtube-dl -x -f 251"
 abbr ytdaxel "youtube-dl --external-downloader axel --external-downloader-args '-n 8'"
 abbr fishedit "~/.config/fish/config.fish"
+abbr lazygit 'git add . && git commit -m "update" && git push origin main'
