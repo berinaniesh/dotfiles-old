366d33e077833c0ca57c3ca7816e974a8f0d0256
= <=>
2021-11-29 16:41:04 +0530

    lazygit_fn

--- user/fish/.config/fish/config.fish
+++ user/fish/.config/fish/config.fish
@@ -9,3 +9,8 @@ abbr ytda "youtube-dl -x -f 251"
 abbr ytdaxel "youtube-dl --external-downloader axel --external-downloader-args '-n 8'"
 abbr fishedit "~/.config/fish/config.fish"
 abbr lazygit 'git add . && git commit -m "update" && git push origin main'
+function gitpush
+	git add .
+	git commit -m "$argv"
+	git push origin main
+	end
