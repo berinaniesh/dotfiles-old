1c63c8c5f8e598607c344bcd67360074b017c73e
= <=>
2021-11-29 16:31:40 +0530

    few fish abbreviations

--- user/fish/.config/fish/config.fish
+++ user/fish/.config/fish/config.fish
@@ -4,3 +4,7 @@ end
 
 set -Ux GOPATH /home/berinaniesh/.golib
 set -Ux PATH $PATH /home/berinaniesh/.golib/bin
+
+abbr ytda "youtube-dl -x -f 251"
+abbr ytdaxel "youtube-dl --external-downloader axel --external-downloader-args '-n 8'"
+abbr fishedit "~/.config/fish/config.fish"
