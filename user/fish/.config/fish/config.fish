4c2b4ffb6582caee1c3e626ae4615475da093a4d
= <=>
2021-12-11 16:35:43 +0530

    update

--- user/fish/.config/fish/config.fish
+++ user/fish/.config/fish/config.fish
@@ -2,13 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
 set -Ux GOPATH /home/berinaniesh/.golib
 set -Ux PATH $PATH /home/berinaniesh/.golib/bin
 
 abbr ytda "youtube-dl -x -f 251"
 abbr ytdaxel "youtube-dl --external-downloader axel --external-downloader-args '-n 8'"
-abbr fishedit "~/.config/fish/config.fish"
+abbr fishedit "vim /home/berinaniesh/.config/fish/config.fish"
 abbr lazygit 'git add . && git commit -m "update" && git push origin main'
+alias compile "g++"
+
 function gitpush
 	git add .
 	git commit -m $argv
