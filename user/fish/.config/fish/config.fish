5df6cea511dd7056e2e877db3e2ec7a56bff1a46
= <=>
2022-02-24 21:15:13 +0530

    update

--- user/fish/.config/fish/config.fish
+++ user/fish/.config/fish/config.fish
@@ -4,13 +4,15 @@ end
 
 
 set -Ux GOPATH /home/berinaniesh/Development/go
-set -Ux PATH $PATH /home/berinaniesh/Development/go/bin
+set -Ux PATH $PATH /home/berinaniesh/Development/go/bin /home/berinaniesh/.bin
 set -Ux EDITOR nvim
 
 abbr ytda "youtube-dl -x -f 251"
 abbr ytdaxel "youtube-dl --external-downloader axel --external-downloader-args '-n 8'"
 abbr fishedit "vim /home/berinaniesh/.config/fish/config.fish"
 abbr lazygit 'git add . && git commit -m "update" && git push origin main'
+abbr swayedit 'vim /home/berinaniesh/.config/sway/config'
+abbr alacrittyedit 'vim /home/berinaniesh/.config/alacritty/alacritty.yml'
 alias compile "g++"
 
 abbr siteupdate "scp -r /home/berinaniesh/Development/berinaniesh.xyz/dst/* berinaniesh@berinaniesh.xyz:/home/berinaniesh/websites/berinaniesh.xyz/"
